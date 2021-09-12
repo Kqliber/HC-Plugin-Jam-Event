7e49df084757da46916401e24ac0776f081dd33c
Kqliber <kqliber@gmail.com>
2021-09-12 01:13:05 +0100

    getPlayer method to convert User back to Player

--- src/main/java/com/github/kqliber/pluginjam/user/User.java
+++ src/main/java/com/github/kqliber/pluginjam/user/User.java
@@ -1,6 +1,8 @@
 package com.github.kqliber.pluginjam.user;
 
 import com.github.kqliber.pluginjam.missions.Mission;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Bukkit;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,5 +17,9 @@ public record User(UUID uuid, List<Mission> missions) {
 
     public User(UUID uuid) {
         this(uuid, new ArrayList<>());
+    }
+
+    public OfflinePlayer getPlayer() {
+        return Bukkit.getOfflinePlayer(uuid);
     }
 }
